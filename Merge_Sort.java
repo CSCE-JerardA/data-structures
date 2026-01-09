1af5faa1e7a0405b63c7cf73284f0054b45372e4
CSCE-JerardA <jerardaustin4@gmail.com>
2026-01-08 23:23:15 -0500

    Still working on code, but I am sleepy

--- Merge_Sort.java
+++ Merge_Sort.java
@@ -2,19 +2,70 @@
 
 public class Merge_Sort {
 
-
+    // Main method with driver
     public static void main(String[] args) {
 
-        int[] numA = {21, 32, 47, 84, 65, 59, 25};
+        // Array containing the data
+        int[] numA = {21, 32, 47, 84, 65, 59, 25, 19};
 
-        System.out.println("");
+        System.out.println("Here is the original array");
+        printArray(numA);
+
+        sorting(numA);
+
+
+
+
         System.out.println("");
 
     }
 
-    private static int mergeSort() {
-        
+    // Method for merging the two arrays together
+    private static void merging(int[] array, int left, int mid, int right) {
 
+        int num1 = mid - left + 1;
+        int num2 = right - mid;
+
+        int[] leftA = new int[num1];
+        int[] rightA = new int[num2];
+
+        for (int i = 0; i < num1; ++i) {
+            leftA[i] = array[left + i];
+        }
+
+        for (int j = 0; j < num2; ++j) {
+            rightA[j] = array[j + mid + 1];
+        }
+
+
+
+
+    }
+
+    // Method for sorting the array 
+    private static void sorting(int[] array, int left, int right) {
+
+        if (left < right){
+
+            // Creates the middle variable 
+            int mid = (left + right) / 2;
+            // Uses recursion to sort the left and mid variables in the array
+            sorting(array, left, mid);
+            // Uses recursion to sort through the mid and right variables in the array
+            sorting(array, mid + 1, right);
+            // Merges the two arrays
+            merging(array, left, mid, right);
+        }
+
+    }
+    // Method for printing/displaying the past and current state of the array 
+    public static void printArray(int[] array) {
+
+        for (int i : array){
+            System.out.println(i + "");
+        }
+
+        System.out.println();
     }
 
 
